d2b00a7077fd2548104a2e862f92cbd92520d004
ducruyy <ducruyy@127.0.0.1>
2016-05-09 15:57:19 +0200

    ajout fonction sur grid, creation imagepanel

--- preprojet_l3_s2/src/Core/Grid.java
+++ preprojet_l3_s2/src/Core/Grid.java
@@ -1,34 +1,45 @@
 package Core;
+
 import java.util.Scanner;
 
 /*
  * La grille des joueurs
  */
-
 /**
  *
  * @author Seawolf
  */
 public class Grid {
+
     private Case[][] grid;
     private int width;
     private int height;
-    
-    public Grid(){
-        this(6,4);
+
+    public Grid() {
+        this(6, 4);
     }
-    
-    public Grid(int x, int y){
+
+    public Grid(int x, int y) {
         this.grid = new Case[x][y];
         this.width = x;
         this.height = y;
+        fillGrid();
     }
-    
-    public int getWidth(){
+
+    private void fillGrid() {
+        for (int i = 0; i < this.getWidth(); i++) {
+            for (int j = 0; j < this.getHeight(); j++) {
+                setStateAtCase(new Coordonate(i, j), State.WAFFLE);
+            }
+        }
+        setStateAtCase(new Coordonate(0, 0), State.POISON);
+    }
+
+    public int getWidth() {
         return this.width;
     }
-    
-    public int getHeight(){
+
+    public int getHeight() {
         return this.height;
     }
 
@@ -39,63 +50,63 @@ public class Grid {
     public void setHeight(int height) {
         this.height = height;
     }
-    
-    public State getStateAtCase(Coordonate c){
+
+    public State getStateAtCase(Coordonate c) {
         return grid[c.getX()][c.getY()].getState();
     }
-    
-    public void setStateAtCase(Coordonate c, State s){
+
+    public void setStateAtCase(Coordonate c, State s) {
         grid[c.getX()][c.getY()].setState(s);
     }
-    
+
     @Override
-    public String toString(){
+    public String toString() {
         String resultat = getWidth() + " " + getHeight();
-        for (int i=0; i<getHeight(); i++) {
+        for (int i = 0; i < getHeight(); i++) {
             resultat += " ";
-            for (int j=0; j<getWidth(); j++)
+            for (int j = 0; j < getWidth(); j++) {
                 switch (grid[i][j].getState()) {
-                case WAFFLE:
-                    resultat += "0";
-                    break;
-                case EATEN:
-                    resultat += "1";
-                    break;
-                case POISON:
-                    resultat += "X";
-                    break;
-                default:
-                    resultat += "?";
+                    case WAFFLE:
+                        resultat += "0";
+                        break;
+                    case EATEN:
+                        resultat += "1";
+                        break;
+                    case POISON:
+                        resultat += "X";
+                        break;
+                    default:
+                        resultat += "?";
                 }
+            }
         }
         return resultat;
     }
-    void load(String textGrid){
+
+    void load(String textGrid) {
         Scanner s = new Scanner(textGrid);
         setWidth(s.nextInt());
         setHeight(s.nextInt());
         this.grid = new Case[getWidth()][getHeight()];
-        for (int i=0; i<getHeight(); i++) {
+        for (int i = 0; i < getHeight(); i++) {
             String ligne;
             ligne = s.next();
-            for (int j=0; j<getWidth(); j++)
+            for (int j = 0; j < getWidth(); j++) {
                 switch (ligne.charAt(j)) {
-                case '0':
-                    grid[i][j].setState(State.WAFFLE);
-                    break;
-                case '1':
-                    grid[i][j].setState(State.EATEN);
-                    break;
-                case 'X':
-                    grid[i][j].setState(State.POISON);
-                    break;
-                default:
-                    grid[i][j].setState(State.WAFFLE);
+                    case '0':
+                        grid[i][j].setState(State.WAFFLE);
+                        break;
+                    case '1':
+                        grid[i][j].setState(State.EATEN);
+                        break;
+                    case 'X':
+                        grid[i][j].setState(State.POISON);
+                        break;
+                    default:
+                        grid[i][j].setState(State.WAFFLE);
                 }
+            }
         }
     }
-        
+
 }
-    
-
-
--- preprojet_l3_s2/src/gui/ImagePanel.java
+++ preprojet_l3_s2/src/gui/ImagePanel.java
@@ -1,35 +1,46 @@
 package gui;
 
-import Core.Grid;
-import java.awt.Graphics;
+import Core.*;
+import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
-public class ImagePanel extends JPanel{
+public class ImagePanel extends JPanel {
 
-    private BufferedImage avail,eaten,poison;
-    private int x,y;
+    private BufferedImage avail, eaten, poison;
+    private int x, y;
     Grid grille;
 
     public ImagePanel() {
-       try {                
-          avail = ImageIO.read(new File("../img/available.png"));
-          eaten = ImageIO.read(new File("../img/eaten.png"));
-          poison = ImageIO.read(new File("../img/poison.png"));
-       } catch (IOException ex) {
+        try {
+            avail = ImageIO.read(new File("../img/available.png"));
+            eaten = ImageIO.read(new File("../img/eaten.png"));
+            poison = ImageIO.read(new File("../img/poison.png"));
+        } catch (IOException ex) {
             // handle exception...
-       }
+        }
+        
+        grille = new Grid(2,2);
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         //draw images
-        
-        g.drawImage(avail, 0, 0, null); // see javadoc for more info on the parameters            
+        for (int i = 0; i < grille.getWidth(); i++) {
+            for (int j = 0; j < grille.getHeight(); j++) {
+                if (grille.getStateAtCase(new Coordonate(i, j)) == State.WAFFLE) {
+                    g.drawImage(avail, i * 100, j * 100, null);
+                }
+                if (grille.getStateAtCase(new Coordonate(i, j)) == State.EATEN) {
+                    g.drawImage(eaten, i * 100, j * 100, null);
+                }
+                if (grille.getStateAtCase(new Coordonate(i, j)) == State.POISON) {
+                    g.drawImage(poison, i * 100, j * 100, null);
+                }
+            }
+        }
     }
-
 }
